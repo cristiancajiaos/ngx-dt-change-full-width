33744b1fbfbf9bcf5217d4f5d71a17ef49e7c73e
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-06-06 09:55:50 -0400

    Instalado paquete NGX-Datatable, creada tabla por defecto

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+  ];
+  columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
+  
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
